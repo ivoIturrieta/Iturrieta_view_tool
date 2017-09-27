cbb1c270dafbcd39c464a809b9d2eb447e6d1045
ivoIturrieta <ivoisalinas@gmail.com>
2017-09-27 01:11:38 -0300

    Ivo tool init commit

---
+++ lib/iturrieta_view_tool.rb
@@ -0,0 +1,5 @@
+require "iturrieta_view_tool/version"
+require "iturrieta_view_tool/renderer"
+module IturrietaViewTool
+  # Your code goes here...
+end
---
+++ lib/iturrieta_view_tool/version.rb
@@ -0,0 +1,3 @@
+module IturrietaViewTool
+  VERSION = "0.1.0"
+end
